7f3f7b2467b6afa9557ead6dd9be211aa28cd92d
Renanz <renan_zelaya@unitec.edu>
2016-05-13 14:35:06 -0600

    codigo del cliente

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author Renan
+ */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+public class Cliente{
+    Socket requestSocket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message,m;
+    Cliente(){}
+    void run()
+    {
+        try{
+            //1. creating a socket to connect to the server
+            requestSocket = new Socket("localhost", 2004);
+            System.out.println("Connected to localhost in port 2004");
+            //2. get Input and Output streams
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(requestSocket.getInputStream());
+            //3: Communicating with the server
+            do{
+                try{
+                    message = (String)in.readObject();
+                    System.out.println("server>" + message);
+                    Scanner s = new Scanner(System.in);
+                    System.out.print("cliente> ");
+                    m = s.next();
+                    sendMessage(m);
+                }
+                catch(ClassNotFoundException classNot){
+                    System.err.println("data received in unknown format");
+                }
+            }while(!message.equals("bye"));
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                requestSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    void sendMessage(String msg)
+    {
+        try{
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("cliente >" + msg);
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+    public static void main(String args[])
+    {
+        Cliente client = new Cliente();
+        client.run();
+    }
+}
